7faa4e4bc1d6e006f118b8f915bfb1eee2e71295
Daniel Arakcheev <darakche@uwaterloo.ca>
2023-02-14 11:47:52 -0500

    finding bug

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin # noqa
+"""Django admin customization"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """Define the amdin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+# provide useradmin as second arg because we want our custom changes to be picked up
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,44 @@
+"""
+Tests for the DJango admin mods
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+class AdminSiteTests(TestCase):
+    """Tests for Django admin"""
+
+    # user camel case for helper function in test class
+    def setUp(self):
+        """Create user and client."""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testpass123'
+        )
+        # every req we make is authed with this user
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User'
+        )
+
+    def test_uses_list(self):
+        """Test that users are listed on page"""
+        url = reverse('admin:core_user_changelist')
+        # http request
+        res = self.client.get(url)
+        
+        self.assertContains(res,self.user.name)
+        self.assertContains(res,self.user.email)
+
+
+    def test_edit_user_page(self):
+        """test the edit user page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code,200)
+        
